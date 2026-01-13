f2d1a938f115d2b47ae0cc3bac17549144ca3911
Adnan-cmrit <adnan810202@gmail.com>
2026-01-13 19:13:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from website import create_app
+
+app = create_app()
+
+@app.route("/")
+def home():
+    return "Hello from main.py + website package!"
+
+if __name__ == "__main__":
+    print("Starting Flask dev server on http://127.0.0.1:5000")
+    app.run(debug=True)
